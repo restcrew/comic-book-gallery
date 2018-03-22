ef6a5ac6b4b39eca670eed0c1e4f340af8fb8129
risto.jauhiainen <risto.jauhiainen@gmail.com>
2018-03-22 19:49:35 +0200

    ComicBooksController uses repo

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -15,6 +15,7 @@ namespace ComicBookGallery.Data
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Id = 1,
                 Artists = new Artist[]
                    {
                         new Artist() {Name = "Dan Slott", Role = "Script"},
@@ -29,6 +30,7 @@ namespace ComicBookGallery.Data
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 657,
                 DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Id = 2,
                 Artists = new Artist[]
                 {
                     new Artist() { Name = "Dan Slott", Role = "Script" },
@@ -44,6 +46,7 @@ namespace ComicBookGallery.Data
                 SeriesTitle = "Bone",
                 IssueNumber = 50,
                 DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                Id = 3,
                 Artists = new Artist[]
                 {
                     new Artist() { Name = "Jeff Smith", Role = "Script" },
@@ -54,11 +57,9 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
         };
-
-
+  
         public ComicBook GetComicBook(int id)
         {
-
 
             ComicBook comicBookToReturn = null;
 
